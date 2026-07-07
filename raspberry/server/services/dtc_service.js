d1f94265e18a1dce4f76c2f837564b680d061024
ITLulu-ux <coruinj@naver.com>
2026-07-07 15:05:41 +0900

    dtc_service.js 수정

--- raspberry/server/services/dtc_service.js
+++ raspberry/server/services/dtc_service.js
@@ -23,7 +23,29 @@ async function saveDTC(dtc) {
 }
 
 /**
- * DTC 전체 조회
+ * ACTIVE DTC 존재 여부
+ */
+async function existsActiveDTC(ecuId, dtcCode) {
+
+    const [rows] = await db.execute(
+
+        `SELECT id
+         FROM dtc_log
+         WHERE ecu_id = ?
+         AND dtc_code = ?
+         AND status='ACTIVE'
+         LIMIT 1`,
+
+        [ecuId, dtcCode]
+
+    );
+
+    return rows.length > 0;
+
+}
+
+/**
+ * DTC 조회
  */
 async function getAllDTC() {
 
@@ -47,14 +69,15 @@ async function updateHeartbeat(ecuId) {
     await db.execute(
 
         `INSERT INTO heartbeat_log
-        (ecu_id, last_received, status)
+        (ecu_id,last_received,status)
 
-        VALUES (?, NOW(), 'CONNECTED')
+        VALUES(?,NOW(),'CONNECTED')
 
         ON DUPLICATE KEY UPDATE
 
-        last_received = NOW(),
-        status = 'CONNECTED'`,
+        last_received=NOW(),
+
+        status='CONNECTED'`,
 
         [ecuId]
 
@@ -82,14 +105,75 @@ async function getHeartbeat() {
 
 }
 
+/**
+ * Heartbeat Timeout 검사
+ */
+async function checkHeartbeatTimeout() {
+
+    const rows = await getHeartbeat();
+
+    for (const ecu of rows) {
+
+        const diff =
+            Date.now() -
+            new Date(ecu.last_received).getTime();
+
+        if (diff > 3000 && ecu.status === 'CONNECTED') {
+
+            await db.execute(
+
+                `UPDATE heartbeat_log
+                 SET status='DISCONNECTED'
+                 WHERE ecu_id=?`,
+
+                [ecu.ecu_id]
+
+            );
+
+            const exists =
+                await existsActiveDTC(
+                    ecu.ecu_id,
+                    'P0702'
+                );
+
+            if (!exists) {
+
+                await saveDTC({
+
+                    ecu_id: ecu.ecu_id,
+
+                    dtc_code: 'P0702',
+
+                    description: 'Heartbeat Timeout',
+
+                    status: 'ACTIVE'
+
+                });
+
+            }
+
+        }
+
+    }
+
+}
+
+/* 1초마다 검사 */
+
+setInterval(checkHeartbeatTimeout,1000);
+
 module.exports = {
 
     saveDTC,
+
+    existsActiveDTC,
 
     getAllDTC,
 
     updateHeartbeat,
 
-    getHeartbeat
+    getHeartbeat,
+
+    checkHeartbeatTimeout
 
 };
